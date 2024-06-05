7dfa2c2c5b2771c9065ad42a0f5e09d4efca15c6
Ammar Haziq <108142876+marziq@users.noreply.github.com>
2024-06-05 16:59:15 +0000

    new file

---
+++ index.js
@@ -0,0 +1,4 @@
+// JavaScript to handle navigation when login button is clicked
+document.getElementById("login-btn").addEventListener("click", function() {
+    window.location.href = "login.html";
+});
